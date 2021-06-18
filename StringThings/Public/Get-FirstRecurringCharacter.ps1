b188dd85cc3096f31c27d2c34126ef92818e58dd
robderickson <rob.derickson@gmail.com>
2021-06-17 22:27:35 -0400

    Added comment-based help. Switched from -contains comparison to -ccontains because Char type does not appear to be case sensitive.

--- StringThings/Public/Get-FirstRecurringCharacter.ps1
+++ StringThings/Public/Get-FirstRecurringCharacter.ps1
@@ -1,7 +1,63 @@
 using namespace System.Collections.Generic
+<#
+.SYNOPSIS
+Get the first recurring character in a string of characters.
+
+.DESCRIPTION
+Given an arbitrary string, Get-FirstRecurringCharacter will return the first character that appears more than once in a string. If a string contains only unique characters, no characters are returned. Instead, depending on the defined WarningAction, you will see a warning that the string contained no recurring characters.
+
+.PARAMETER String
+A string to check for recurring characters.
+
+.EXAMPLE
+This command takes the string "Hello World" and returns the letter 'l':
+
+Get-FirstRecurringCharacter -String 'Hello World'
+
+l
+
+.EXAMPLE
+This command takes the string "Hello World" and returns the letter 'l':
+
+Get-FirstRecurringCharacter -String 'Hello World'
+l
+
+.EXAMPLE
+This command takes the string "Hello World" from the pipeline and returns the letter 'm':
+
+'Lorem lipsum' | Get-FirstRecurringCharacter
+m
+
+.EXAMPLE
+This example create a custom object with Name and String properties, and pipes them to Get-FirstRecurringCharacter. It returns a space character, because that is the first recurring character in the String property. Checking the count of the returned characters confirms a character was returned:
+
+$Example = [PSCustomObject]@{
+    Name = 'Sample Text'
+    String = 'The quick brown fox jumps over the lazy dog.'
+}
+$Example | Get-FirstRecurringCharacter
+ 
+($Example | Get-FirstRecurringCharacter).Count
+1
+
+.EXAMPLE
+This example results in a warning because the string does not contain any recurring characters:
+
+Get-FirstRecurringCharacter -String 'Algeria' -WarningAction Continue
+WARNING: The provided string does not contain recurring characters.
+
+.NOTES
+This function is case sensitive. Because the characters 'A' and 'a' are unique
+#>
 function Get-FirstRecurringCharacter {
     [CmdletBinding()]
     param(
+        [Parameter(
+            Mandatory=$true,
+            ValueFromPipeline=$true,
+            ValueFromPipelineByPropertyName=$true,
+            ValueFromRemainingArguments=$true,
+            HelpMessage='Provide a string to find the first recurring character')]
         [string]$String
     )
 
@@ -13,7 +69,7 @@ function Get-FirstRecurringCharacter {
         
         # For each character in the character array, check if the character already appears in checked characters
         foreach ($char in $CharArray) {
-            if ($CheckedChars -contains $char) {
+            if ($CheckedChars -ccontains $char) {
                 # If character already in the list checked characters, exit the loop and return the character.
                 return $char
             } else {
@@ -21,5 +77,8 @@ function Get-FirstRecurringCharacter {
                 $CheckedChars.Add($char)
             }
         }
+
+        # If the loop completes without returning, let the user know
+        Write-Warning -Message 'The provided string does not contain recurring characters.'
     }
 }
