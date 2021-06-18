c47561d8c311741bab080c316266b55cdd1aa9f2
robderickson <rob.derickson@gmail.com>
2021-06-17 21:46:58 -0400

    Added comments and parameter attributes. Added warning if the loop completes without finding a recurring character.

--- StringThings/Public/Get-FirstRecurringCharacter.ps1
+++ StringThings/Public/Get-FirstRecurringCharacter.ps1
@@ -6,14 +6,18 @@ function Get-FirstRecurringCharacter {
     )
 
     PROCESS {
+        # Convert the string into a character array
         $CharArray = $String.ToCharArray()
-        
+        # Create an empty List to keep the characters already checked
         $CheckedChars = [List[Char]]::new()
         
+        # For each character in the character array, check if the character already appears in checked characters
         foreach ($char in $CharArray) {
             if ($CheckedChars -contains $char) {
+                # If character already in the list checked characters, exit the loop and return the character.
                 return $char
             } else {
+                # If character is not in the list of checked characters, add it to the list and continue the loop.
                 $CheckedChars.Add($char)
             }
         }
